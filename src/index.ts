1a6c83ba61ea2d208ddf9e91f794eaf7f6daad97
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2024-05-01 11:57:22 +0800

    responsive

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import {Leaderboard} from "@/scenes/leaderboard/leaderboard";
  */
 class Game extends Engine {
   constructor() {
-    super({ displayMode: DisplayMode.FillScreen,width:1280, height:720, canvasElementId: 'game'});
+    super({ displayMode: DisplayMode.FitScreen,width:1280, height:720, canvasElementId: 'game'});
   }
 
   public start() {
